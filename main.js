f67c358d7324e8915aaa6e8ba635f1bee6f9e139
ChambeauEnzo <maryse.gosselin68@gmail.com>
2023-11-15 10:13:42 +0100

    exo1-2

---
+++ main.js
@@ -0,0 +1,10 @@
+var MavariableString = 'toto';
+
+var MavariableNumerique = 100;
+
+var MavariableTabeau = ['Apple', 'Banana'];
+
+var MavariableObjet = window
+
+alert(MavariableString + MavariableNumerique + MavariableTabeau[0]+
+    MavariableObjet.location.pathname);
