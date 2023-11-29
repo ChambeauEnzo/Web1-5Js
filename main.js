c72ccc3ba288c10495d007396271243bc5cc20cd
ChambeauEnzo <maryse.gosselin68@gmail.com>
2023-11-29 10:36:56 +0100

    exercice 7

--- main.js
+++ main.js
@@ -2,7 +2,17 @@ var somme =0;
 let nombre= prompt("Veuillez saisir un nombre pour calculer le factorielle:")
 
 for (let i =1; i < nombre ; i++) {
- somme =i + somme ;
+ /*somme =i + somme ; */
+ somme = addition(i ,somme);
  console.log(somme);
 
 }
+
+function addition (a,b) {
+    let c = 0;
+    c = a + b
+    return c;
+    
+    }
+
+    
