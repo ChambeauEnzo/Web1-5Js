ed93859ee95b72c55e21e92b31dae306a19bdeae
ChambeauEnzo <maryse.gosselin68@gmail.com>
2023-12-13 08:31:52 +0100

    exercice 8 debut

--- main.js
+++ main.js
@@ -1,18 +1,7 @@
-var somme =0;
-let nombre= prompt("Veuillez saisir un nombre pour calculer le factorielle:")
-
-for (let i =1; i < nombre ; i++) {
- /*somme =i + somme ; */
- somme = addition(i ,somme);
- console.log(somme);
-
+function ModifText(letext) {
+    alert ("je vais modifier la div");
 }
 
-function addition (a,b) {
-    let c = 0;
-    c = a + b
-    return c;
-    
-    }
+ModifText("Hello");
 
-    
+
