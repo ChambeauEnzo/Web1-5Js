13900aef4a2065000f068e77286074060a2288a8
ChambeauEnzo <maryse.gosselin68@gmail.com>
2023-12-13 08:52:18 +0100

    exercice 10

--- main.js
+++ main.js
@@ -1,7 +1,44 @@
-function ModifText(letext) {
-    alert ("je vais modifier la div");
+var compteur = 0;
+var compteur2 = 0;
+
+var Madiv = document.getElementById("Methode2");
+Madiv.addEventListener("click", UneProcedureQuiChangeLetext);
+
+Madiv = document.getElementById("Methode3");
+Madiv.addEventListener("mouseover", UneProcedureQuiSurvole);
+
+Madiv = document.getElementById("Methode4");
+Madiv.addEventListener("keydown", UneProcedureQuiEcrit);
+
+function UneProcedureQuiEcrit(evenement) {
+evenement.target.innerHTML = "On a appuyé sur : "+evenement.key;
 }
 
-ModifText("Hello");
+function UneProcedureQuiChangeLetext(evenement) {
+    compteur++;
+    evenement.target.innerHTML = "on m'a cliqué dessus "+compteur+" fois";
+    }
 
+    function UneProcedureQuiSurvole(evenement) {
+        compteur2++;
+        evenement.target.innerHTML = "on m'a survollé "+compteur2+" fois";
+    }
 
+    // Récupérer l'élément div
+var monDiv = document.getElementById("monDiv");
+
+// Ajouter un écouteur d'événement pour le clic de la souris
+monDiv.addEventListener("click", function() {
+  // Changer la classe CSS de l'élément div
+  monDiv.className = "nouvelleClasse";
+});
+
+function changerVisibilite(visible) {
+    var div = document.getElementById("monDiv2");
+    if (visible) {
+      div.style.visibility = "visible";
+    } else {
+      div.style.visibility = "hidden";
+    }
+  }
+  
